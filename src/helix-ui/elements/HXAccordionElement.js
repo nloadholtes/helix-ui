c63e62741d43018776f72271e9683b581754bd9d
thiagu <thiagu.nagappan@rackspace.com>
2018-02-13 10:17:04 -0600

    feat(steppers): allow toggling between steps

--- src/helix-ui/elements/HXAccordionElement.js
+++ src/helix-ui/elements/HXAccordionElement.js
@@ -11,20 +11,38 @@ export class HXAccordionElement extends HXElement {
 
     constructor () {
         super();
+        this._toggleStep = this._toggleStep.bind(this);
     }
 
     connectedCallback () {
-        var openedPanel = this.getAttribute('selectedPanel');
-        this._target = document.querySelector('hx-accordion-panel[id="'+openedPanel+'"]');
-        this._target.open=!this._target.open;
+        this._openedPanelId = this.getAttribute('selectedPanel');
+        if (this._openedPanelId) {
+          this._target = document.querySelector('hx-accordion-panel[id="'+this._openedPanelId+'"]');
+          this._target.open=!this._target.open;
+          document.querySelector('hx-accordion').addEventListener('click', this._toggleStep)
+        }
     }
 
     attributeChangedCallback (attr, oldVal, newVal) {
       this._target = document.querySelector('hx-accordion-panel[id="'+oldVal+'"]');
       this._target.open=!this._target.open;
-      // this._reveal = this.shadowRoot.querySelector('hx-reveal');
-      // this._reveal.setAttribute('aria-expanded', this.open);
-      // this._reveal.setAttribute('open', '');
       console.log('Panel -attribute changed');
+    }
+
+    _toggleStep() {
+      this._newPanelId = this.getAttribute('selectedPanel');
+      if (this._openedPanelId !== this._newPanelId) {
+        this._oldTarget = document.querySelector('hx-accordion-panel[id="'+this._openedPanelId+'"]');
+        this._oldReveal = this._oldTarget.shadowRoot.querySelector('hx-reveal');
+        this._oldDisclosure = this._oldTarget.shadowRoot.querySelector('hx-disclosure');
+        //Close the old Panel
+        this._oldTarget.open=false;
+        this._oldReveal.setAttribute('aria-expanded', false);
+        this._oldDisclosure.setAttribute('aria-expanded', false);
+        this._oldReveal.removeAttribute('open');
+        //reset the panelId variable
+        this._openedPanelId = this._newPanelId;
+      }
+
     }
 }
